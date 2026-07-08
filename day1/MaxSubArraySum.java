ee3a631a5e852a56439b88c8ff685b91df93fd0a
PRASHANT SAINBHI <sainbhison002@gmail.com>
2026-07-08 09:43:52 +0530

    Add Day 1 Java solutions

---
+++ day1/MaxSubArraySum.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class MaxSubArraySum  {
+
+    public static int maxSubArrayBrute(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                maxSum = Math.max(maxSum, currentSum);
+            }
+        }
+
+        return maxSum;
+    }
+
+}
+
+
+// max sub array sum
---
+++ day1/optimisedmaxsub.java
@@ -0,0 +1,17 @@
+package day1;
+public class optimisedmaxsub {
+
+    public static int kadanes(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
+
+// optimised max subarray sum
