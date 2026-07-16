175952a2f799f783c20208a40105a2e94b7b3599
PRASHANT SAINBHI <sainbhison002@gmail.com>
2026-07-16 10:40:19 +0530

    day 7 Min Removalsm

---
+++ day7/minRemovals.java
@@ -0,0 +1,31 @@
+package day7;
+
+public class minRemovals {
+    static class Solution {
+        public int minRemovals(int n, int m, int[] arrN, int[] arrM) {
+            int[] k = arrM;
+            int[] cnt= new int[m+1];
+            int sumK=0;
+            for(int x: k) sumK+=x;
+            int preSat=0;
+            for(int i=0; i<m;i++){
+                if(k[i]==0) preSat++;
+            }
+            int left=0, satisfied=preSat, minLen=Integer.MAX_VALUE;
+            for(int right=0; right<n;right++){
+                int c= arrN[right];
+                cnt[c]++;
+                if(cnt[c]==k[c-1]) satisfied++;
+                while(satisfied==m){
+                    minLen=Math.min(minLen, right-left+1);
+                    int lc= arrN[left];
+                    cnt[lc]--;
+                    if(cnt[lc]==k[lc-1]-1) satisfied--;
+                    left++;
+                }
+            }
+
+            return (minLen==Integer.MAX_VALUE) ? -1 : (minLen-sumK);
+        }
+    }
+}
